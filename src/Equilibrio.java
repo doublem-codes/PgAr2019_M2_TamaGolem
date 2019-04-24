39a1f0887c8b430e3cb3158b77c0b5dd3b21d0f0
doublem-codes <m.milanesi004@studenti.unibs.it>
2019-04-24 21:41:01 +0200

    v4.0

--- src/Equilibrio.java
+++ src/Equilibrio.java
@@ -8,6 +8,7 @@ public class Equilibrio {
 
 
     public void generaEquilibrio() {
+
         inserisciElementi(5);//partita.getNumeroElementi()
         printMatrix(iterazioneElementi);
 
@@ -19,7 +20,7 @@ public class Equilibrio {
         iterazioneElementi = new int[numElementi][numElementi];
         //danneggia
         for (int i = 0; i < numElementi; i++) {
-            iterazioneElementi[i] = danneggia(numElementi);
+            iterazioneElementi[i] = danneggia(iterazioneElementi[i]);
         }
 
         //subisce
@@ -35,9 +36,8 @@ public class Equilibrio {
             }
         }
         //sistema
-        for (int i = 0; i < numElementi; i++) {
-            iterazioneElementi[i] = sistemaPotenze(iterazioneElementi[i]);
-        }
+
+            iterazioneElementi = sistemaPotenze(iterazioneElementi);
 
 
         return false;
@@ -65,24 +65,30 @@ public class Equilibrio {
         return sum;
     }
 
-    private int[] danneggia(int numElementi) {
+    private int[] danneggia(int[] array) {
 
-        int[] array = new int[numElementi];
 
-        int numElementiDanneggia = random.nextInt(numElementi);
-        if (numElementiDanneggia == 0) {
-            numElementiDanneggia = 1;
-        }
-
+        int numElementiDanneggia = random.nextInt(array.length-1)+1;
+        int numMaxPotenza = 0;
+        int pos1 = 0, pos2 =0;
+        int numElementi=array.length;
         do {
-            int numMaxPotenza = 0;
-            int pos = 0;
+            numMaxPotenza = numElementi-1;
+            pos1 = random.nextInt(array.length);
+            if(array[pos1]==0){
+                array[pos1] = random.nextInt(numMaxPotenza) + 1;
+            }else{
+                if(pos1==0){
+                    pos2=pos1+1;
+                }else{
+                    pos2=pos1-1;
+                }
 
-            numMaxPotenza = (numElementi - numElementiDanneggia);
-            pos = random.nextInt(numElementi);
-            array[pos] = random.nextInt(numMaxPotenza) + 1;
-            numElementiDanneggia--;
+                array[pos2] = random.nextInt(numMaxPotenza) + 1;
+            }
+
             numElementi--;
+            numElementiDanneggia--;
 
         } while (numElementiDanneggia != 0);
 
@@ -101,34 +107,57 @@ public class Equilibrio {
 
             int numMaxPotenza = 0;
             for (int i = 0; i < array.length; i++) {
+                numMaxPotenza = array.length -(numDanneggia);
                 if (array[i] == 0) {
-                    numMaxPotenza = (numDanneggia);
-                    array[i] = -(random.nextInt(numMaxPotenza) + 1);
-                    numDanneggia--;
+
+                    if(numMaxPotenza!=0){
+                        array[i] = -(random.nextInt(numMaxPotenza) + 1);
+                    }else {
+                        array[i]=-1;
+                    }
+
+                        numMaxPotenza--;
                 }
             }
-            //completare
-       if(sommaArrayPositivi(array)+sommaArrayNegativi(array)==0){
-
-       }
         return array;
 
     }
 
-    private int[] sistemaPotenze(int[] array){
-            int posCambio=random.nextInt(array.length);
-            if(array[posCambio]==0){
-             if(posCambio>=0 && posCambio<(array.length)){
-                 posCambio=posCambio+1;
-             }else if(posCambio == array.length){
-                 posCambio=posCambio-1;
-             }
+    private int[][] sistemaPotenze(int[][] matrix){
+            int posCambio=0;
+            for(int i=0;i<matrix.length;i++){
+                if(sommaArrayNegativi(matrix[i])+sommaArrayPositivi(matrix[i])==0){
+                    i++;
+                }else{
 
+                    //cerco se ci sono più elementi che danneggiano o che subiscono
+                    //tra uno dei due trovo il maggiore e lo sostituisco
+                    int nSubisce=0, nDanneggia=0;
+                    for(int l=0; l<matrix[i].length;l++){
+                        if(matrix[i][l]>0){
+                            nDanneggia++;
+                        }else{
+                            nSubisce++;
+                        }
+                    }
+
+                    if(nDanneggia>nSubisce){
+                        posCambio=cercaMaggiore(matrix[i],false);
+                    }else{
+                        posCambio=cercaMaggiore(matrix[i],true);
+                    }
+                    matrix[i][posCambio]=0;
+                    matrix[i][posCambio]=-(sommaArrayNegativi(matrix[i])+sommaArrayPositivi(matrix[i]));
+
+
+
+                }
             }
-            array[posCambio]=0;
-            array[posCambio]=-(sommaArrayNegativi(array)+sommaArrayPositivi(array));
 
-        return array;
+
+
+
+        return matrix;
     }
 
     private boolean printMatrix(int[][] matrix){
@@ -141,5 +170,26 @@ public class Equilibrio {
         return false;
     }
 
+    private int cercaMaggiore(int[] array, boolean isSubisce){
+        int max=array[0];
+        int index=0;
+        if(isSubisce){
+            for (int i=0; i<array.length;i++){
+                if(array[i]<0 && array[i]<max){
+                    max=array[i];
+                    index=i;
+                }
+            }
+        }else{
+            for (int i=0; i<array.length;i++){
+                if(array[i]>0 && array[i]>max){
+                    max=array[i];
+                    index=i;
+                }
+            }
+        }
+
+        return index;
+    }
 
 }
